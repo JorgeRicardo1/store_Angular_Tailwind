63888004b614803891c9f216b1946e56613bad81
Jorge <jorge.ricardo@utp.edu.co>
2024-01-04 21:47:12 -0500

    Product component terminado

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductComponent } from './../../components/product/product.component'
+import { ProductComponent } from './../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
+
 
 @Component({
   selector: 'app-list',
@@ -10,6 +12,36 @@ import { ProductComponent } from './../../components/product/product.component'
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] =[
+      {
+        id: Date.now(),
+        title: 'product1',
+        price: 100,
+        image: 'https://picsum.photos/230/230?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'product2',
+        price: 100,
+        image: 'https://picsum.photos/230/230?r=15',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'product3',
+        price: 100,
+        image: 'https://picsum.photos/230/230?r=18',
+        creationAt: new Date().toISOString()
+      }
+    ];
+
+    this.products.set(initProducts);
+  }
 
   fromChild(event: string) {
     console.log('estamos en el padre');
--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, SimpleChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
@@ -10,6 +11,8 @@ import { Component, Input, SimpleChanges } from '@angular/core';
 export class CounterComponent {
   @Input({required: true}) duration: number = 0;
   @Input({required: true}) message: string = '';
+  counter = signal(0);
+  counterRef: number | undefined;
 
   constructor(){
     console.log('constructor');
@@ -21,6 +24,39 @@ export class CounterComponent {
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+    const duration = changes['duration'];
+    console.log(duration);
+    if(duration && duration.currentValue !== duration.previousValue) {
+      this.doSomething();
+    }
+  }
 
+  ngOnInit(){
+    //after render
+    //async, then, subs
+    console.log('ngOninit')
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message => ', this.message);
+    this.counterRef = window.setInterval(() => {
+      this.counter.update(statePrev => statePrev + 1)
+    }, 1000)
+  }
+
+  ngAfterViewInit() {
+    //after render
+    //hijos ya fueron rendered
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
+    window.clearInterval(this.counterRef);
+  }
+
+  doSomething() {
+    console.log('change duration');
   }
 }
