09473c3b0a02976c47ea5f05f3fc3883967fdcf9
Jorge <jorge.ricardo@utp.edu.co>
2024-01-17 19:58:32 -0500

    ruta y metodo chunks terminado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
-import { ListComponent } from '@product/pages/list/list.component';
-import { AboutComponent } from './domains/shared/pages/about/about.component';
+// import { ListComponent } from '@product/pages/list/list.component';
+// import { AboutComponent } from './domains/shared/pages/about/about.component';
 import { LayoutComponent } from './domains/shared/components/layout/layout.component'
 import { NotFoundComponent } from './domains/info/pages/not-found/not-found.component';
-import { ProductDetailComponent } from '@product/pages/product-detail/product-detail.component';
+// import { ProductDetailComponent } from '@product/pages/product-detail/product-detail.component';
 
 
 
@@ -15,15 +15,18 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        component: ListComponent
+        loadComponent: () => import('@product/pages/list/list.component')
+        // component: ListComponent
       },
       {
         path: 'about',
-        component: AboutComponent
+        loadComponent: () => import('./domains/shared/pages/about/about.component')
+        // component: AboutComponent
       },
       {
         path: 'product/:productId',
-        component: ProductDetailComponent
+        loadComponent: () => import('@product/pages/product-detail/product-detail.component')
+        // component: ProductDetailComponent
       }
     ]
   },
